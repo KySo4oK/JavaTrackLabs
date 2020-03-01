6ef3c213c17e850a2d566fb305ed05495ddc3e8b
KySo4oK <prostorostuk08@gmail.com>
2020-03-01 20:32:27 +0200

    wrote all methods in Model class

--- lab1/src/main/java/javaTrack/model/Model.java
+++ lab1/src/main/java/javaTrack/model/Model.java
@@ -4,16 +4,72 @@ public class Model {
     private Animal[] animals;
 
     public Animal[] getAnimalsByMinAge(int minAge) {
+        int quantity = getQuantityOfAnimalsByParameters(minAge);
+        Animal[] animalsByParameter = new Animal[quantity];
+        int index = 0;
+        for (Animal animal : animals) {
+            if (animal.getAge() >= minAge) {
+                animalsByParameter[index] = animal;
+                index++;
+            }
+        }
+        return animalsByParameter;
+    }
 
-        return null;
+    private int getQuantityOfAnimalsByParameters(int minAge) {
+        int quantity = 0;
+        for (Animal animal : animals) {
+            if (animal.getAge() >= minAge) {
+                quantity++;
+            }
+        }
+        return quantity;
     }
 
     public Animal[] getAnimalsByFamily(String family) {
-        return null;
+        int quantity = getQuantityOfAnimalsByParameters(family);
+        Animal[] animalsByParameter = new Animal[quantity];
+        int index = 0;
+        for (Animal animal : animals) {
+            if (animal.getFamily().equals(family)) {
+                animalsByParameter[index] = animal;
+                index++;
+            }
+        }
+        return animalsByParameter;
+    }
+
+    private int getQuantityOfAnimalsByParameters(String family) {
+        int quantity = 0;
+        for (Animal animal : animals) {
+            if (animal.getFamily().equals(family)) {
+                quantity++;
+            }
+        }
+        return quantity;
     }
 
     public Animal[] getAnimalsByAgeAndColoring(int age, String coloring) {
-        return null;
+        int quantity = getQuantityOfAnimalsByParameters(age, coloring);
+        Animal[] animalsByParameter = new Animal[quantity];
+        int index = 0;
+        for (Animal animal : animals) {
+            if ((animal.getFamily().equals(coloring)) && (animal.getAge() == age)) {
+                animalsByParameter[index] = animal;
+                index++;
+            }
+        }
+        return animalsByParameter;
+    }
+
+    private int getQuantityOfAnimalsByParameters(int age, String coloring) {
+        int quantity = 0;
+        for (Animal animal : animals) {
+            if ((animal.getFamily().equals(coloring)) && (animal.getAge() == age)) {
+                quantity++;
+            }
+        }
+        return quantity;
     }
 
 }
