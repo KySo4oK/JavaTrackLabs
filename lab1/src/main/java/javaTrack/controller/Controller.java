2174f8e36d6506856516dffd8d04e9b3c8c6d385
KySo4oK <prostorostuk08@gmail.com>
2020-05-04 15:38:04 +0300

    move almost all input methods to Utility Controller

--- lab1/src/main/java/javaTrack/controller/Controller.java
+++ lab1/src/main/java/javaTrack/controller/Controller.java
@@ -1,9 +1,6 @@
 package javaTrack.controller;
 
 import javaTrack.model.Model;
-import javaTrack.model.exception.ImpossibleColoringIndexException;
-import javaTrack.model.exception.ImpossibleFamilyIndexException;
-import javaTrack.model.exception.NegativeAgeException;
 import javaTrack.view.TextConstant;
 import javaTrack.view.View;
 
@@ -36,10 +33,10 @@ public class Controller {
                 int scanValue = scanner.nextInt();
                 switch (scanValue) {
                     case 1:
-                        inputMinAgeAndPrintAnimalsByMinAge();
+                        printAnimalsByMinAge();
                         break;
                     case 2:
-                        inputIndexOfFamilyAndPrintAnimalsByIndexOfFamilies(model.getFamilies());
+                        printAnimalsByIndexOfFamilies(model.getFamilies());
                         break;
                     case 3:
                         printAnimalsByAgeAndColoring(); //todo move all input methods to another class
@@ -67,114 +64,33 @@ public class Controller {
             return;
         }
         while (true) {
-            scanner.nextLine();
-            view.printMessage(TextConstant.PRINT_FILE_PATH);
-            String filePath;
-            if (scanner.hasNextLine()) {
-                filePath = scanner.nextLine();
-                try {
-                    model.saveCurrentAnimalsToFile(filePath);
-                } catch (IOException e) {
-                    view.printMessage(e.getMessage());
-                    continue;
-                }
-                view.printMessage(TextConstant.ANIMALS_WAS_SAVED);
-                return;
+            try {
+                model.saveCurrentAnimalsToFile(utilityController.inputFilePath());
+            } catch (IOException e) {
+                view.printMessage(e.getMessage());
+                view.printMessage(TextConstant.ANIMALS_WAS_NOT_SAVED);
+                continue;
             }
-//            scanner.nextLine();
-            view.printMessage(TextConstant.ANIMALS_WAS_NOT_SAVED);
+            view.printMessage(TextConstant.ANIMALS_WAS_SAVED);
+            return;
         }
 
     }
 
     private void printAnimalsByAgeAndColoring() {
-        view.printAnimals(model.getAnimalsByAgeAndColoring(inputAge(),
-                inputColoring(model.getColorings())));
+        view.printAnimals(model.getAnimalsByAgeAndColoring(utilityController.inputAge(),
+                utilityController.inputColoring(model.getColorings())));
     }
 
-    private String inputColoring(String[] colorings) {
-        while (true) {
-            view.printFindByColoringMenu(colorings);
-            if (scanner.hasNextInt()) {
-                int index = scanner.nextInt();
-                try {
-                    Validator.isPossibleIndexForColorings(colorings.length, index);
-                } catch (ImpossibleColoringIndexException e) {
-                    view.printMessage(e.getMessage());
-                    continue;
-                }
-                return colorings[index];
-            }
-            scanner.nextLine();
-            view.printMessage(TextConstant.WRONG_INPUT);
-        }
-    }
-
-    private int inputAge() {
-        while (true) {
-            view.printMessage(TextConstant.PRINT_AGE);
-            if (scanner.hasNextInt()) {
-                int age = scanner.nextInt();
-                try {
-                    Validator.checkAgeForNegative(age);
-                } catch (NegativeAgeException e) {
-                    view.printMessage(TextConstant.WRONG_INPUT);
-                    view.printMessage(e.getMessage());
-                    continue;
-                }
-                return age;
-            } else {
-                scanner.nextLine();
-                view.printMessage(TextConstant.WRONG_INPUT);
-            }
-        }
-    }
-
-    private void inputIndexOfFamilyAndPrintAnimalsByIndexOfFamilies(String[] families) {
-        while (true) {
-            view.printFindByFamilyMenu(families);
-            if (scanner.hasNextInt()) {
-                int index = scanner.nextInt();
-                try {
-                    Validator.isPossibleIndexForColorings(families.length, index);
-                } catch (ImpossibleFamilyIndexException e) {
-                    view.printMessage(TextConstant.WRONG_INPUT);
-                    view.printMessage(e.getMessage());
-                    continue;
-                }
-                printAnimalsByIndexOfFamilies(index);
-                return;
-            }
-            scanner.nextLine();
-        }
+    private void printAnimalsByIndexOfFamilies(String[] families) {
+        printAnimalsByIndexOfFamilies(utilityController.inputIndexOfFamily(families));
     }
 
     private void printAnimalsByIndexOfFamilies(int index) {
         view.printAnimals(model.getAnimalsByFamily(model.getFamilies()[index]));
     }
 
-    private void inputMinAgeAndPrintAnimalsByMinAge() {
-        while (true) {
-            view.printMessage(TextConstant.PRINT_MIN_AGE);
-            if (scanner.hasNextInt()) {
-                int minAge = scanner.nextInt();
-                try {
-                    Validator.checkAgeForNegative(minAge);
-                } catch (NegativeAgeException e) {
-                    view.printMessage(TextConstant.WRONG_INPUT);
-                    view.printMessage(e.getMessage());
-                    continue;
-                }
-                printAnimalsByMinAge(minAge);
-                return;
-            } else {
-                scanner.nextLine();
-                view.printMessage(TextConstant.WRONG_INPUT);
-            }
-        }
-    }
-
-    private void printAnimalsByMinAge(int minAge) {
-        view.printAnimals(model.getAnimalsByMinAge(minAge));
+    private void printAnimalsByMinAge() {
+        view.printAnimals(model.getAnimalsByMinAge(utilityController.inputMinAge()));
     }
 }
--- lab1/src/main/java/javaTrack/controller/UtilityController.java
+++ lab1/src/main/java/javaTrack/controller/UtilityController.java
@@ -1,5 +1,9 @@
 package javaTrack.controller;
 
+import javaTrack.model.exception.ImpossibleColoringIndexException;
+import javaTrack.model.exception.ImpossibleFamilyIndexException;
+import javaTrack.model.exception.NegativeAgeException;
+import javaTrack.view.TextConstant;
 import javaTrack.view.View;
 
 import java.util.Scanner;
@@ -13,5 +17,92 @@ public class UtilityController {
         this.scanner = new Scanner(System.in);
     }
 
+    int inputAge() {
+        while (true) {
+            view.printMessage(TextConstant.PRINT_AGE);
+            if (scanner.hasNextInt()) {
+                int age = scanner.nextInt();
+                try {
+                    Validator.checkAgeForNegative(age);
+                } catch (NegativeAgeException e) {
+                    view.printMessage(TextConstant.WRONG_INPUT);
+                    view.printMessage(e.getMessage());
+                    continue;
+                }
+                return age;
+            } else {
+                scanner.nextLine();
+                view.printMessage(TextConstant.WRONG_INPUT);
+            }
+        }
+    }
+
+    String inputColoring(String[] colorings) {
+        while (true) {
+            view.printFindByColoringMenu(colorings);
+            if (scanner.hasNextInt()) {
+                int index = scanner.nextInt();
+                try {
+                    Validator.isPossibleIndexForColorings(colorings.length, index);
+                } catch (ImpossibleColoringIndexException e) {
+                    view.printMessage(e.getMessage());
+                    continue;
+                }
+                return colorings[index];
+            }
+            scanner.nextLine();
+            view.printMessage(TextConstant.WRONG_INPUT);
+        }
+    }
+
+    int inputMinAge() {
+        while (true) {
+            view.printMessage(TextConstant.PRINT_MIN_AGE);
+            if (scanner.hasNextInt()) {
+                int minAge = scanner.nextInt();
+                try {
+                    Validator.checkAgeForNegative(minAge);
+                } catch (NegativeAgeException e) {
+                    view.printMessage(TextConstant.WRONG_INPUT);
+                    view.printMessage(e.getMessage());
+                    continue;
+                }
+                return minAge;
+            } else {
+                scanner.nextLine();
+                view.printMessage(TextConstant.WRONG_INPUT);
+            }
+        }
+    }
+
+    int inputIndexOfFamily(String[] families) {
+        while (true) {
+            view.printFindByFamilyMenu(families);
+            if (scanner.hasNextInt()) {
+                int index = scanner.nextInt();
+                try {
+                    Validator.isPossibleIndexForColorings(families.length, index);
+                } catch (ImpossibleFamilyIndexException e) {
+                    view.printMessage(TextConstant.WRONG_INPUT);
+                    view.printMessage(e.getMessage());
+                    continue;
+                }
+                return index;
+            }
+            scanner.nextLine();
+        }
+    }
+
+    String inputFilePath() {
+        while (true) {
+            scanner.nextLine();
+            view.printMessage(TextConstant.PRINT_FILE_PATH);
+            if (scanner.hasNextLine()) {
+                return scanner.nextLine();
+            }
+//            scanner.nextLine();
+            view.printMessage(TextConstant.ANIMALS_WAS_NOT_SAVED);
+        }
+    }
 
 }
