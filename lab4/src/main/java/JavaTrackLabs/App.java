5de4ea350180cd544d875299e15a84313d8c63df
KySo4oK <prostorostuk08@gmail.com>
2020-05-05 21:28:01 +0300

    finish 4 lab

--- lab4/src/main/java/JavaTrackLabs/App.java
+++ lab4/src/main/java/JavaTrackLabs/App.java
@@ -1,13 +1,49 @@
 package JavaTrackLabs;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        List<Integer> list = generateArrayList(15);
+        System.out.println("List - " + list);
+        int value = inputValue();
+        System.out.println("Nearest - " + getNearestToValue(list, value));
+    }
+
+    private static int getNearestToValue(List<Integer> list, int value) {
+        int smallestDifference = Math.abs(list.get(0) - value);
+        int nearestIndex = 0;
+
+        for (int i = 0; i < list.size(); i++) {
+            int currentDifference = Math.abs(list.get(i) - value);
+            if (smallestDifference > currentDifference) {
+                smallestDifference = currentDifference;
+                nearestIndex = i;
+            }
+        }
+        return list.get(nearestIndex);
+    }
+
+    private static int inputValue() {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.println("Print value");
+            if (scanner.hasNextInt()) {
+                return scanner.nextInt();
+            } else {
+                System.out.println("Wrong input");
+                scanner.nextLine();
+            }
+        }
+    }
+
+    private static List<Integer> generateArrayList(int number) {
+        List<Integer> list = new ArrayList<Integer>();
+        for (int i = 0; i < number; i++) {
+            list.add((int) (Math.random() * 100));
+        }
+        return list;
     }
 }
