dfa3c31f2c9782f69041e4a5e0a2102f78af0324
KySo4oK <prostorostuk08@gmail.com>
2020-05-19 13:34:10 +0300

    config file for logging add "clean" dependencies

--- lab5/src/main/java/javaTrack/model/DataSource.java
+++ lab5/src/main/java/javaTrack/model/DataSource.java
@@ -8,30 +8,11 @@ import java.io.IOException;
 public class DataSource {
     private static final String FILE = "data.json";
 
-    public Animal[] getAnimals() {
-        Animal[] animals = new Animal[10];
-        for (int i = 0; i < animals.length; i++) {
-            animals[i] = new Animal("type" + i,
-                    "classOfAnimals" + i,
-                    "family" + i % 3,
-                    "lineage" + i,
-                    "kind" + i,
-                    "subspecies" + i,
-                    i % 3 + 1,
-                    "coloring" + i % 3);
-        }
-        return animals;
-    }
-
     public Animal[] getAnimalsFromFile() throws IOException {
-        Animal[] animals = null;
-        ObjectMapper mapper = new ObjectMapper();
-        animals = mapper.readValue(new File(FILE), Animal[].class);
-        return animals;
+        return new ObjectMapper().readValue(new File(FILE), Animal[].class);
     }
 
     public void saveAnimals(Animal[] animals, String filePath) throws IOException {
-        ObjectMapper mapper = new ObjectMapper();
-        mapper.writeValue(new File(filePath), animals);
+        new ObjectMapper().writeValue(new File(filePath), animals);
     }
 }
